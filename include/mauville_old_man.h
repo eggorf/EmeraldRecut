3f7214e5452daeb580afc95442e5d291c5356a42
eggorf <83192338+eggorf@users.noreply.github.com>
2026-02-26 23:19:14 -0800

    old man finish

--- include/mauville_old_man.h
+++ include/mauville_old_man.h
@@ -1,27 +1,6 @@
 #ifndef GUARD_MAUVILLE_OLD_MAN_H
 #define GUARD_MAUVILLE_OLD_MAN_H
 
-struct BardSound
-{
-    u16 length;
-    u16 pitch;
-};
-
-struct BardSong
-{
-    u8 lyricsIndex;
-    u8 soundIndex;
-    u8 timer;
-    u8 state;
-    s16 length; // Length of the sound for the word currently being sung (i.e. the sum of 'length' in all the current word's phonemes).
-    u16 volume;
-    s16 pitch;
-    s16 voiceInflection;
-    u16 lyrics[NUM_BARD_SONG_WORDS];
-    struct BardSound sounds[0];
-    const struct BardSoundTemplate *soundTemplates;
-};
-
 void SetMauvilleOldMan(void);
 u8 GetCurrentMauvilleOldMan(void);
 void SetMauvilleOldManObjEventGfx(void);
